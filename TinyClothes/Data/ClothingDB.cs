55d5b3634a9ab2815eb699ca70f017e90d91fad3
arcum-omni <travis.l.eiler@gmail.com>
2020-01-23 11:02:17 -0800

    Partial Commit, Add not fully functional.

---
+++ TinyClothes/Data/ClothingDB.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TinyClothes.Models;
+
+namespace TinyClothes.Data
+{
+    /// <summary>
+    /// Contains DB Helper methods for <see cref="Models.Clothing"/>
+    /// </summary>
+    public static class ClothingDB
+    {
+        public static List<Clothing> GetAllClothing()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Adds clothing object to database
+        /// returns object with id populated
+        /// </summary>
+        /// <param name="c"></param>
+        /// <returns></returns>
+        public static Clothing Add(Clothing c, StoreContext context)
+        {
+            context.Add(c);       // Prepares insert query
+            context.SaveChanges();// Execute insert query
+
+            return c;
+        }
+    }
+}
