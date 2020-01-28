80bb26c0dd62eeebffd6947d3f467063c75529f8
arcum-omni <hut8dev@gmail.com>
2020-01-28 09:27:53 -0800

    Added ClothesController

--- TinyClothes/Controllers/ClothesController.cs
+++ TinyClothes/Controllers/ClothesController.cs
@@ -3,11 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using TinyClothes.Data;
 
 namespace TinyClothes.Controllers
 {
     public class ClothesController : Controller
     {
+        private readonly StoreContext _context;
+
+        public ClothesController(StoreContext context)
+        {
+            // this._context = context; // "this" is optional
+            _context = context; 
+        }
+
         [HttpGet]
         public IActionResult ShowAll()
         {
