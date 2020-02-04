569a6a20bbce58094ad6e0c9c58fe64e06213cab
arcum-omni <hut8dev@gmail.com>
2020-02-04 10:15:50 -0800

    Added Data Annotations for validating clothing

--- TinyClothes/Models/Clothing.cs
+++ TinyClothes/Models/Clothing.cs
@@ -20,32 +20,40 @@ namespace TinyClothes.Models
         /// <summary>
         /// Clothing Size
         /// </summary>
+        [Required(ErrorMessage = "Size Required")]
         public string Size { get; set; }
 
         /// <summary>
         /// Type of clothing; shirt, pants, etc
         /// </summary>
+        [Required]
         public string Type { get; set; }
 
         /// <summary>
         /// Primary color of outfit
         /// </summary>
+        [Required]
         public string Color { get; set; }
 
         /// <summary>
         /// Retail Price of the item
         /// </summary>
+        [Range(0.0, 5000.0)]
         public double Price { get; set; }
 
         /// <summary>
-        /// Clothing Title
+        /// Clothing Title,
+        /// For pattern recognition go to https://regexr.com/
         /// </summary>
+        [Required(ErrorMessage = "Title Required")]
+        [StringLength(35)]
+        //[RegularExpression("^([A-Za-z0-9])+$")] // Example of regular expression RegEx validation, great in some cases.
         public string Title { get; set; }
 
         /// <summary>
         /// Clothing item description
         /// </summary>
+        [StringLength(800)]
         public string Description { get; set; }
-
     }
 }
