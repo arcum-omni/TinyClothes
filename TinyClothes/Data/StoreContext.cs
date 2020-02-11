8c592a63b198ae4199f02b328db4fd102be8a08f
arcum-omni <hut8dev@gmail.com>
2020-02-11 10:49:52 -0800

    Created accounts and added to DB context

--- TinyClothes/Data/StoreContext.cs
+++ TinyClothes/Data/StoreContext.cs
@@ -17,5 +17,7 @@ namespace TinyClothes.Data
         // https://docs.microsoft.com/en-us/aspnet/core/data/ef-mvc/intro?view=aspnetcore-3.1#create-the-database-context
         public DbSet<Clothing> Clothing { get; set; }
 
+        // need a migration to create an account table
+        public DbSet<Account> Accounts { get; set; }
     }
 }
---
+++ TinyClothes/Models/Account.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TinyClothes.Models
+{
+    /// <summary>
+    /// A single user account.
+    /// </summary>
+    public class Account
+    {
+        [Key]
+        public int AccountID { get; set; }
+
+        [Required]
+        [StringLength(24)]
+        public string UserName { get; set; }
+
+        [StringLength(60)]
+        public string FullName { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)] //<input type="password">
+        [StringLength(150)]
+        public string Password { get; set; }
+
+        [Required]
+        [EmailAddress]
+        [StringLength(120)]
+        public string Email { get; set; }
+
+        public string Address { get; set; }
+
+        // https://en.wikipedia.org/wiki/Children%27s_Online_Privacy_Protection_Act
+        [DataType(DataType.Date)]
+        public DateTime? DateOfBirth { get; set; }
+    }
+}
