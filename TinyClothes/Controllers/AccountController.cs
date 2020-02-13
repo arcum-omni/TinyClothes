65730ca6a962dc29baf917432a5b5000500607c0
Travis Eiler <travis.l.eiler@gmail.com>
2020-02-13 10:55:57 -0800

    Registration page#31 (#34)
    * Created accounts and added to DB context
    
    * Created RegisterViewModel
    
    * Created Register Page
    
    * Basic Registration Implemented

---
+++ TinyClothes/Controllers/AccountController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using TinyClothes.Data;
+using TinyClothes.Models;
+
+namespace TinyClothes.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly StoreContext _context;
+
+        public AccountController(StoreContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterViewModel reg)
+        {
+            if (ModelState.IsValid)
+            {
+                // check username & email are not taken, ie unique
+                if (!await AccountDb.IsUserNameTaken(reg.UserName, _context))
+                {
+                    Account acc = new Account()
+                    {
+                        Email = reg.Email,
+                        FullName = reg.FullName,
+                        Password = reg.Password,
+                        UserName = reg.UserName
+                    };
+
+                    // add acc to DB
+                    await AccountDb.Register(acc, _context);
+                    return RedirectToAction("Index", "Home");
+                    // redirect to homepage
+                }
+                else // if username is taken, add error msg
+                {
+                    ModelState.AddModelError(nameof(Account.UserName), "Username is taken, choose a different username.");
+                }
+            }
+
+            return View(reg);
+        }
+    }
+}
---
+++ TinyClothes/Models/Account.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TinyClothes.Models
+{
+    /// <summary>
+    /// A single user account.
+    /// </summary>
+    public class Account
+    {
+        [Key]
+        public int AccountID { get; set; }
+
+        [Required]
+        [StringLength(24)]
+        public string UserName { get; set; }
+
+        [StringLength(60)]
+        public string FullName { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)] //<input type="password">
+        [StringLength(150)]
+        public string Password { get; set; }
+
+        [Required]
+        [EmailAddress]
+        [StringLength(150)]
+        public string Email { get; set; }
+
+        public string Address { get; set; }
+
+        // https://en.wikipedia.org/wiki/Children%27s_Online_Privacy_Protection_Act
+        [DataType(DataType.Date)]
+        public DateTime? DateOfBirth { get; set; }
+    }
+
+    /// <summary>
+    /// View Model, look up Panopto @ approx 19min for description
+    /// </summary>
+    public class RegisterViewModel
+    {
+        // Account ID auto generated
+
+        [Required]
+        [StringLength(60)]
+        [Display(Name = "Full Name")]
+        public string FullName { get; set; }
+
+        [Required]
+        [StringLength(24)]
+        [Display(Name = "Username")]
+        public string UserName { get; set; }
+
+        [Required]
+        [StringLength(150)]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Compare(nameof(Password))] // we could hard code [Compare("Password")], but that does not have refactoring support
+        [Display(Name = "Confirm Password")]
+        public string ConfirmPassword { get; set; }
+
+        [Required]
+        [EmailAddress]
+        [StringLength(150)]
+        public string Email { get; set; }
+    }
+}
