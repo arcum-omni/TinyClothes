5551f9f974cd0c466e04956f72942c0f60c97a5c
arcum-omni <hut8dev@gmail.com>
2020-02-13 09:20:05 -0800

    Created RegisterViewModel

--- TinyClothes/Models/Account.cs
+++ TinyClothes/Models/Account.cs
@@ -28,7 +28,7 @@ namespace TinyClothes.Models
 
         [Required]
         [EmailAddress]
-        [StringLength(120)]
+        [StringLength(150)]
         public string Email { get; set; }
 
         public string Address { get; set; }
@@ -36,5 +36,36 @@ namespace TinyClothes.Models
         // https://en.wikipedia.org/wiki/Children%27s_Online_Privacy_Protection_Act
         [DataType(DataType.Date)]
         public DateTime? DateOfBirth { get; set; }
+    }
+
+    /// <summary>
+    /// View Model, look up Panopto @ approx 19min for description
+    /// </summary>
+    public class RegisterViewModel
+    {
+        // Account ID auto generated
+
+        [Required]
+        [StringLength(60)]
+        public string FullName { get; set; }
+
+        [Required]
+        [StringLength(24)]
+        public string UserName { get; set; }
+
+        [Required]
+        [StringLength(150)]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Compare(nameof(Password))] // we could hard code [Compare("Password")], but that does not have refactoring support
+        public string ConfirmPassword { get; set; }
+
+        [Required]
+        [EmailAddress]
+        [StringLength(150)]
+        public string Email { get; set; }
     }
 }
