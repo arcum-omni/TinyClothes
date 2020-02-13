44bd39b9e4f859fde76db712a2030295cde44337
arcum-omni <hut8dev@gmail.com>
2020-02-13 10:52:14 -0800

    Basic Registration Implemented

---
+++ TinyClothes/Data/AccountDb.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using TinyClothes.Data;
+using TinyClothes.Models;
+
+namespace TinyClothes
+{
+    public static class AccountDb
+    {
+        public static async Task<bool> IsUserNameTaken(string username, StoreContext context)
+        {
+            bool isTaken = await (from acc in context.Accounts where username == acc.UserName select acc).AnyAsync();
+            
+            return isTaken;
+        }
+
+        public static async Task<Account> Register(Account acc, StoreContext context)
+        {
+            await context.Accounts.AddAsync(acc);
+            await context.SaveChangesAsync();
+
+            return acc;
+        }
+    }
+}
