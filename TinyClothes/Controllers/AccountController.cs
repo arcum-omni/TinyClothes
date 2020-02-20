2279d3e923a7265e3821bd822b88ed44a8ceb76a
arcum-omni <hut8dev@gmail.com>
2020-02-20 10:41:27 -0800

    Got logout almost working

--- TinyClothes/Controllers/AccountController.cs
+++ TinyClothes/Controllers/AccountController.cs
@@ -86,5 +86,11 @@ namespace TinyClothes.Controllers
 
             return View(login);
         }
+
+        public IActionResult Logout()
+        {
+            SessionHelper.DestroyUserSession(_http);
+            return RedirectToAction("Index", "Home");
+        }
     }
 }
