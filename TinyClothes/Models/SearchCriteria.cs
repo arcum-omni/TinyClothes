80cf4279a657b701854a3cc9667f8a296c012a1a
arcum-omni <hut8dev@gmail.com>
2020-03-03 10:21:49 -0800

    created search criteria model

---
+++ TinyClothes/Models/SearchCriteria.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TinyClothes.Models
+{
+    public class SearchCriteria
+    {
+        /// <summary>
+        /// No Arg Constructor
+        /// </summary>
+        public SearchCriteria()
+        {
+            SearchResults = new List<Clothing>();
+        }
+
+        public string Size { get; set; }
+
+        /// <summary>
+        /// The type of clothing: shirt, pants, hat, etc.
+        /// </summary>
+        public string Type { get; set; }
+
+        public string Title { get; set; }
+
+        public double? MinPrice { get; set; }
+
+        public double? MaxPrice { get; set; }
+
+        public List<Clothing> SearchResults { get; set; }
+    }
+}
